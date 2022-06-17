38aaeda1040f3b6288d3dd96ab8627e033b99406
Ilkin Guluzada <glzdreal@gmail.com>
2022-06-18 01:13:52 +0400

    Yerdeyishme

---
+++ yeni_tasklar/Yerdeyishme/app.js
@@ -0,0 +1,8 @@
+let eded1 = 51;
+let eded2 = 123;
+alert(`Ədədin dəyişməmiş halı:\n1. Ədəd: ${eded1}\n2. Ədəd: ${eded2}`)
+eded1=eded1+eded2;
+eded2=eded1-eded2;
+eded1=eded1-eded2;
+alert(`Ədədin dəyişməmiş halı:\n1. Ədəd: ${eded1}\n2. Ədəd: ${eded2}`)
+
